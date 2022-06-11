ac6e5a5252bf247252971be2f7aae0805f5f225a
sunwanchung <sunwanchung@gmail.com>
2022-06-11 23:37:06 +0900

    슬라이드 px->viewport

--- JS/mdiaJS.js
+++ JS/mdiaJS.js
@@ -1,9 +1,9 @@
 //자동새로고침 함수
 const lines = document.querySelector('.lines')
 
-window.onresize = function () {
+/*window.onresize = function () {
   document.location.reload();
 };
 if (matchMedia("screen and (max-width: 1000px)").matches){
   console.log('hello')
-}
+}*/
