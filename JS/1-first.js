0feaed184ff954c6a3322e084dedeb17080f372e
sunwanchung <sunwanchung@gmail.com>
2022-06-14 12:35:33 +0900

    수정완료

--- JS/1-first.js
+++ JS/1-first.js
@@ -2,9 +2,14 @@ const items1 = document.querySelector('.item1')
 const items2 = document.querySelector('.item2')
 const items3 = document.querySelector('.item3')
 const items4 = document.querySelector('.item4')
+const icon = document.querySelector('.icon')
 
 const appstore = document.querySelector('.apple')
 const playstore = document.querySelector('.play')
+
+icon.addEventListener('click',function(){
+  window.location = 'https://supersmc1.github.io/project-reborn/'
+})
 
 window.addEventListener("scroll",function(event){
 let scrollY = this.scrollY
