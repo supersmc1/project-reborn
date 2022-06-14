58b4d433d322e70188d31129ceacc8dd1f158480
sunwanchung <sunwanchung@gmail.com>
2022-06-14 14:31:57 +0900

    완료

--- JS/2-second.js
+++ JS/2-second.js
@@ -3,3 +3,30 @@ const momo = document.querySelector('.momo')
 momo.addEventListener('click',function(){
   window.location = "https://supersmc1.github.io/project-News.reborn/"
 })
+
+const news1 = document.querySelector('.news1')
+const news2 = document.querySelector('.news2')
+const news3 = document.querySelector('.news3')
+const news4 = document.querySelector('.news4')
+const news5 = document.querySelector('.news5')
+
+news1.addEventListener('click',function(){
+  window.location = 'https://supersmc1.github.io/project-newsreborn/'
+})
+
+news2.addEventListener('click',function(){
+  window.location = 'https://supersmc1.github.io/project-newsreborn2/'
+})
+
+news3.addEventListener('click',function(){
+  window.location = 'https://supersmc1.github.io/project-newsreborn3/'
+})
+
+news4.addEventListener('click',function(){
+  window.location = 'https://supersmc1.github.io/project-newsreborn4/'
+})
+
+news5.addEventListener('click',function(){
+  window.location = 'https://supersmc1.github.io/project-newsreborn5/'
+})
+
